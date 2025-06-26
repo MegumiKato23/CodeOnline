cfd1879f78a4773558559eba8be34fb27e7f5f6b
cptll <2058634652@qq.com>
2025-06-26 17:51:38 +0800

    分享功能bug修复:无法正常访问

--- Client/src/stores/userStore.ts
+++ Client/src/stores/userStore.ts
@@ -1,3 +1,4 @@
+import { set } from 'lodash-es';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
@@ -52,8 +53,21 @@ export const useUserStore = defineStore('user', () => {
   };
 
   const logout = () => {
+    // 清除登录状态
     isLoggedIn.value = false;
-    // 登出时清除权限
+
+    // 清除用户信息
+    username.value = '';
+    account.value = '';
+    avatar.value = '';
+    status.value = '';
+    createAt.value = '';
+    userId.value = '';
+
+    // 清除项目相关状态
+    currentProjectId.value = null;
+
+    // 清除权限
     clearPermissions();
   };
 
