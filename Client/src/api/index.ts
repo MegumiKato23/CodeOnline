601acb9d9a6e1e898dacb708d9cbbf6dfe700eff
echotaoyx <yxt18834828951@163.com>
2025-06-28 18:48:34 +0800

    正确Redis

--- Client/src/api/index.ts
+++ Client/src/api/index.ts
@@ -264,6 +264,32 @@ class ApiClient {
     );
     return response.data;
   }
+  // 修改 saveCode 方法
+  async saveCode(data: {
+    userId: string;
+    files: Array<{
+      name: string;
+      path: string;
+      content: string;
+      type: FileType;
+    }>;
+  }): Promise<any> {
+    return this.client.post('http://localhost:3001/api/code/save', data);
+  }
+
+  // 修改 getCode 方法
+  async getCode(userId: string): Promise<{
+    data: {
+      files: Array<{
+        name: string;
+        path: string;
+        content: string;
+        type: FileType;
+      }>;
+    };
+  }> {
+    return this.client.get(`http://localhost:3001/api/code/${userId}`);
+  }
 }
 
 export const api = new ApiClient();
@@ -292,9 +318,4 @@ interface CodeData {
   css: string;
   js: string;
 }
-export default {
-  saveCode: (data: { userId: string; html: string; css: string; js: string }) =>
-    axios.post('http://localhost:3001/api/code/save', data),
 
-  getCode: (userId: string) => axios.get<CodeData>(`http://localhost:3001/api/code/${userId}`),
-};
